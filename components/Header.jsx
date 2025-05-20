bcaefd1dd0db1807d6770d6c334bb85ba4b1be75
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-20 15:37:57 +0200

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,41 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="container">
+        <img src="src/assets/img/dc-logo.png"></img>
+        <ul>
+          <li>
+            <a href="#">CHARACTER</a>
+          </li>
+          <li>
+            <a href="#">COMICS</a>
+          </li>
+          <li>
+            <a href="#">MOVIES</a>
+          </li>
+          <li>
+            <a href="#">TV</a>
+          </li>
+          <li>
+            <a href="#">GAMES</a>
+          </li>
+          <li>
+            <a href="#"> COLLECTIBLES</a>
+          </li>
+          <li>
+            <a href="#">VIDEOS</a>
+          </li>
+          <li>
+            <a href="#">FANS</a>
+          </li>
+          <li>
+            <a href="#">NEWS</a>
+          </li>
+          <li>
+            <a href="#">SHOP</a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
