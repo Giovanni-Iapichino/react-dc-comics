ea8005ed88310db4de2c8b40c9d0dcd7a2691324
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-20 17:30:44 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
+import IconBar from "../components/Iconbar";
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <IconBar />
     </>
   );
 }
