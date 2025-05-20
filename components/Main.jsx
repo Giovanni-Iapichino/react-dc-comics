0cccde91d129c3a83131bd2f8cde4d37f262c8de
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-05-20 16:54:53 +0200

    main-dc

---
+++ components/Main.jsx
@@ -0,0 +1,13 @@
+export default function Main() {
+  return (
+    <main id="page-main">
+      <div className="container">
+        <section className="main-content">
+          <h1>
+            --{">"} Content goes here {"<"}--
+          </h1>
+        </section>
+      </div>
+    </main>
+  );
+}
